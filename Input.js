f6177358270b4b448ebac589516e1e41abbeb993
xaropnho <brunoramos1709@gmail.com>
2025-09-03 02:26:53 -0300

    Delete Input.js

--- Input.js
+++
@@ -1,5 +0,0 @@
-function calcularRaiz() {
-    let numero = parseInt(document.getElementById("numero").value);
-    let raiz = Math.sqrt(numero)
-    document.getElementById("raiz").value = raiz.toFixed(2)
-}
